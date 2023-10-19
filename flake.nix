87e9d624051bae72876bc70b9138c2988fcf2e62
nxy7 <lolnoxy@gmail.com>
2023-10-20 00:13:26 +0200

    casper contract initiated
    Signed-off-by: nxy7 <lolnoxy@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Project starter";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nix2container.url = "github:nlewo/nix2container";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = { nixpkgs.follows = "nixpkgs"; };
+    };
+
+  };
+
+  outputs = { flake-parts, nixpkgs, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+      perSystem = { config, system, ... }:
+        let
+          overlays = [ (import inputs.rust-overlay) ];
+          pkgs = import nixpkgs { inherit system overlays; };
+        in {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              (rust-bin.selectLatestNightlyWith (toolchain:
+                toolchain.default.override {
+                  extensions = [ "rust-src" ];
+                  targets = [ "wasm32-unknown-unknown" ];
+                }))
+              bun
+              cmake
+              pkg-config
+              # openssl.dev
+            ];
+          };
+        };
+    };
+}
+
